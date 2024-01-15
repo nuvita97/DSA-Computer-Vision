4b5812001b8317f05ef9ccb7c44e42d2e3fa4493
Viet-Thai NGUYEN <vietthai2303@gmail.com>
2024-01-15 21:05:33 +0100

    train with yolov5 as baseline

--- Lab_02/app/interface.py
+++ Lab_02/app/interface.py
@@ -6,25 +6,9 @@ from keras.models import load_model
 from keras.preprocessing.image import img_to_array
 
 
-# Load and detect animals
-# def detect_animals(image):
-#     model = YOLO("runs/detect/train7/weights/best.pt")
-#     results = model.predict(image)
-#     result = results[0]
-#     output = []
-#     for box in result.boxes:
-#         x1, y1, x2, y2 = [
-#           round(x) for x in box.xyxy[0].tolist()
-#         ]
-#         class_id = box.cls[0].item()
-#         prob = round(box.conf[0].item(), 2)
-#         output.append([
-#           x1, y1, x2, y2, result.names[class_id], prob
-#         ])
-#     return output
-
+# Function to detect object from the best YOLOv8 model
 def detect_animals(image):
-    model = YOLO("runs/detect/train7/weights/best.pt")
+    model = YOLO("runs/detect/train8/weights/best.pt")
     results = model.predict(image)
     result = results[0]
     
@@ -40,9 +24,7 @@ def detect_animals(image):
         class_id = box.cls[0].item()
         prob = round(box.conf[0].item(), 2)
         label = f"{result.names[class_id]}: {prob}"
-
-        # Get color for class
-        color = color_map.get(class_id, "white")  # Default to white if class ID not in color map
+        color = color_map.get(class_id, "white") 
 
         # Draw bounding box and label
         draw.rectangle([(x1, y1), (x2, y2)], outline=color, width=2)
@@ -51,32 +33,21 @@ def detect_animals(image):
     return image
 
 
+# Streamlit interface
 st.title('Animal Detection App')
 
 st.write("This app can detect monkeys, cats, and dogs.")
 
 uploaded_file = st.file_uploader("Choose an image...", type="jpg")
 
-# if uploaded_file is not None:
-#     image = Image.open(uploaded_file)
-#     st.image(image, caption='Uploaded Image.', use_column_width=True)
-#     st.write("")
-#     # st.write("Classifying...")
-#     processed_image = detect_animals(image)
-#     st.image(processed_image, caption='Processed Image.', use_column_width=True)
-
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
     st.write("Detection result.")
 
-    processed_image = image.copy()  # Create a copy of the original image
+    processed_image = image.copy()  
     processed_image = detect_animals(processed_image)
 
-    # Create two columns
+    # Set 2 images on 2 horizontal columns
     col1, col2 = st.columns(2)
-
-    # Display the original image in the first column
     col1.image(image, caption='Uploaded Image.', use_column_width=True)
-
-    # Display the processed image in the second column
     col2.image(processed_image, caption='Processed Image.', use_column_width=True)
